102759c62a17fc8e2ee33e8968062a9e1937e2a5
Your NameAnisse69 <anisse.ttv@gmail.com>
2025-03-22 08:45:01 +0100

    ajustement et ajout de commentaire

--- mon_site/scripts.js
+++ mon_site/scripts.js
@@ -1,31 +1,17 @@
-function effacer(value_defaut, id) {
-  truk = document.getElementById(id);
-  type = truk.tagName;
-  type = type.toLowerCase();
-  if (type == "input") {
-    if (truk.value == value_defaut) truk.value = "";
-  } else if (type == "textarea") {
-    if (truk.innerHTML == value_defaut) truk.innerHTML = "";
-  }
-}
-
-function changeColor() {
-  b1.classList.toggle("click");
-}
-
 // Fonction pour générer les confettis
-function throwConfetti() {
-  confetti({
-    particleCount: 100, // Nombre de confettis
-    spread: 70, // Angle de dispersion
-    origin: { y: 0.6 }, // Position de départ des confettis
-  });
-}
+function tConfetti() {
+    confetti({
+      particleCount: 150,  
+      spread: 90,          
+      origin: { y: 0.6 },  
+      colors: ['#ff6347', '#32cd32', '#1e90ff'],  
+      shapes: ['square', 'circle'], 
+      gravity: 0.7        
+    });
+  }
 
 // Ajouter l'événement de clic sur le bouton
 document
   .getElementById("celebrateBtn")
-  .addEventListener("click", throwConfetti);
+  .addEventListener("click", tConfetti);
 
-const b1 = document.getElementById("b1");
-b1.addEventListener("click", changeColor);
